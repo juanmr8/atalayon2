0014e0711c81ad34dbf5f710b4b9b944e86699c6
juanmr8 <info@juanmoraromero.com>
2024-03-06 11:34:48 -0300

    first commit

---
+++ Documents/Sites/Projects/movin-coil/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className='flex justify-center items-center h-[100vh] bg-slate-800 text-slate-100'>
+      <div className='flex flex-col justify-center items-center '>
+        <p className='uppercase text-[80px]'>{`Movin'Coil`}</p>
+        <p className='uppercase text-[19px]'>{`Comin' Soon`}</p>
+      </div>
+    </div>
+  );
+}
